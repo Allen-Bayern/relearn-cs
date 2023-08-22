08d4931f47d8e574a59a451a0ba1a9bd0bb8b7d9
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-08-22 08:56:52 +0800

    feat: 待解决

---
+++ src/MergeIntervals/solution.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def solution(intervals: list[list[int]]) -> list[list[int]]:
+    res: list[list[int]] = list()
+    
+    return res
+
+if __name__ == '__main__':
+    print(solution([[1,3],[2,6],[8,10],[15,18]]))
