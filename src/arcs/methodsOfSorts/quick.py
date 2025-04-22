2f78e6d028dc15ffe99c9266f5f5fc8483ba6169
yueb <nobodyplay@yeah.net>
2025-04-22 23:26:23 +0800

    perf: 重新迭代快排

--- src/arcs/methodsOfSorts/quick.py
+++ src/arcs/methodsOfSorts/quick.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def quick_sort(arr):
-    if (not arr) or len(arr) == 1:
-        # 如果arr是空数组或单元素数组，那么直接return它自身即可
-        return arr
 
-    # 选中基准点为最右一位
-    pivot = arr[-1]
-    # 建立空数组left和right
-    left, right = list(), list()
+def quick_sort(arr: list[int]) -> list[int]:
+    """
+    快排
+    """
+    arr_length = len(arr)
+    if arr_length <= 1:
+        return arr.copy()
 
-    for elem in arr[:len(arr) - 1:]:
-        # 像上面这样写可以保证不遍历到最后一位
-        if elem <= pivot:
-            left.append(elem)
+    left: list[int] = list()
+    right: list[int] = list()
+    pivot = arr[0]
+
+    for i in range(1, arr_length):
+        if arr[i] < pivot:
+            left.append(arr[i])
         else:
-            right.append(elem)
+            right.append(arr[i])
 
-    return quick_sort(left) + [pivot] + quick_sort(right)
+    return quick_sort(left).extend([pivot]).extend(quick_sort(right))
 
-if __name__ == '__main__':
-	print(quick_sort([2,1,5,3,8,4,9,5]))
+
+if __name__ == "__main__":
+    print(quick_sort([2, 1, 5, 3, 8, 4, 9, 5]))
